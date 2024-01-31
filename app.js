1094ce9beaa08d0dc47a713eb60cf055f321c7f0
jeffreyC4 <jeffreycorderony@gmail.com>
2024-01-31 15:38:16 -0500

    provider testing

--- app.js
+++ app.js
@@ -24,7 +24,7 @@ app.use(session({
   }),
   secret:process.env.SESSION_SECRET,
   resave:false,
-  saveUninitialized:true,
+  saveUninitialized:false,
   cookie: { secure: process.env.NODE_ENV === 'production', httpOnly: false, maxAge: 1000 * 60 * 60 }
 }));
 
